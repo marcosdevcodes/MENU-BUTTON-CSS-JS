ee5555347698a4aa0c5ea586d9503ab3742ff8d1
marcosdevcodes <marcosdevcodes@gmail.com>
2024-08-07 00:30:42 -0300

    Projeto de Menu button

---
+++ main.js
@@ -0,0 +1,8 @@
+function btnAnimar(){
+    const btn = document.getElementById("btn-menu");
+    btn.classList.toggle('active');
+    //Esse toggle serve para verificar se
+    //ja existe ou não a class active
+    // se existir ele remove se não existir
+    // ele adiciona a class active
+}
